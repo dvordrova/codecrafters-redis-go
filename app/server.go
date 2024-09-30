62756de4d4b58966a9376100152dbc2b0254e1ba
Dmitry Bozhko <meridos.programs@yandex.ru>
2024-09-30 21:25:29 +0400

    once ping

--- app/server.go
+++ app/server.go
@@ -1,27 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
+	"log/slog"
 	"net"
 	"os"
+	"unsafe"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+const (
+	port         = 6379
+	pingRequest  = "*1\r\n$4\r\nPING\r\n"
+	pingResponse = "+PONG\r\n"
+	bufSize      = 1000
+)
 
 func main() {
+	logger := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+	slog.SetDefault(slog.New(logger))
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		log.Fatalf("Failed to bind to port %d", port)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	buff := make([]byte, bufSize)
+	for {
+		err := func() error {
+			conn, err := l.Accept()
+			if err != nil {
+				return fmt.Errorf("error accepting connection: %w\n", err)
+			}
+			slog.Debug("new connection established")
+			defer conn.Close()
+			n, err := conn.Read(buff)
+			// TODO: read all, set timeout
+			if err != nil {
+				return fmt.Errorf("failed to read incoming request: %v\n", err)
+			}
+			cmd := buff[:n]
+			slog.Debug(fmt.Sprint(cmd))
+			if bytes.Equal(cmd, unsafe.Slice(unsafe.StringData(pingRequest), len(pingRequest))) {
+				n, err := conn.Write(unsafe.Slice(unsafe.StringData(pingResponse), len(pingResponse)))
+				if err != nil {
+					return fmt.Errorf("failed to write response: %w", err)
+				}
+				if n != len(pingResponse) {
+					return fmt.Errorf("writing ping response resulted in %d bytes, but expected %d", n, len(pingResponse))
+				}
+			}
+			return nil
+		}()
+		if err != nil {
+			slog.Warn("connection processing error", "err", err)
+		}
 	}
 }
